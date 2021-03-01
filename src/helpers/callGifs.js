1a5360fe3f44972cc80f8fcd8fdb46d181cce064
ezleal1987 <ezleal1987@gmail.com>
2021-03-01 18:06:09 -0300

    Modificacion de Http por Https

--- src/helpers/callGifs.js
+++ src/helpers/callGifs.js
@@ -1,7 +1,7 @@
     
     
     export const getGif = async(category)=>{
-        const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category) }&limit=20&api_key=XdV2OXrO5KxQJiFcJAFriDTK99IDJYfs`;
+        const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category) }&limit=20&api_key=XdV2OXrO5KxQJiFcJAFriDTK99IDJYfs`;
         const resp = await fetch( url );
         const {data} = await resp.json();
         const dataMap = data.map (img => {
